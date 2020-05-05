f4d7b5374a17cee956cb23cc6a475164d831ce77
Berlin2611 <juli.mora.5667@gmail.com>
2020-05-05 17:05:10 -0500

    carpeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GamesService } from './services/games.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GameListComponent,
+    GameFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
+    FormsModule
   ],
-  providers: [],
+  providers: [GamesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
